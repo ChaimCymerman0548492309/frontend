655b4d36c65a6216d80b24f52e8e5f020c447112
Your Name <you@example.com>
2023-11-20 09:58:42 +0200

    f

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,36 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+// import { GlobalState } from "./state";
+// import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+// import Product from "./pages/Product";
+// import Products from "./pages/Products";
+// import Login from "./pages/Login";
+// import Sign from "./pages/Sign";
+// import Cart from "./pages/Cart";
+// import Compare from "./pages/Compare";
+// import { useSelector } from "react-redux";
+// import IsraelMap from "./pages/Map";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Assuming that your state structure has a "global" property
+  // const data = useSelector((state: { global: GlobalState }) => state.global.products);
+  // console.log(data);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <BrowserRouter>
+        {/* <CssBaseline /> */}
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            {/* <Route path="/products" element={<Products />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/Sign" element={<Sign />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
-
+export default App;
